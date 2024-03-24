2643355afb9ce548437b060cdfc2aaacb6682123
Menachem Adin <meniadin@gmail.com>
2024-03-24 18:19:17 +0200

    Name temp files

--- test/logger_test/logger_test.cpp
+++ test/logger_test/logger_test.cpp
@@ -10,59 +10,124 @@
 using namespace std;
 using namespace testing;
 
-#define LOGGER_MERGE_STDERR_TO_STDOUT
 
 class OutputRedirection
 {
-    int stdoutBackup, stderrBackup, stdoutFile, stderrFile;
-    inline static const char constStdoutFileName[] = "stdout_XXXXXX";
-    inline static const char constStderrFileName[] = "stderr_XXXXXX";
-    char stdoutFileName[sizeof(constStdoutFileName)];
-    char stderrFileName[sizeof(constStderrFileName)];
+public:
+    static inline const string tempFileSep{"_"};
+    static inline const string tempFileExtension = ".log";
+
+private:
+    bool separateStderr, keepTempFiles;
+
+    string tempFilePrefix;
+
+    int stdoutFDBackup, stderrFDBackup;
+    int stdoutFD, stderrFD, stdoutAndStderrFD;
+    string stdoutTempFileName, stderrTempFileName, stdoutAndStderrTempFileName;
+    FILE *stdoutTempFile, *stderrTempFile, *stdoutAndStderrTempFile;
+    static inline const string creatingTempFileMode{"w"};
+    static constexpr ios_base::openmode readingTempFileMode = ios::in;
 
 public:
-    void SetUp()
+    OutputRedirection(const string tempFilePrefix, bool separateStderr=false, bool keepTempFiles=false) :
+        tempFilePrefix{tempFilePrefix}, separateStderr{separateStderr}, keepTempFiles{keepTempFiles}
     {
-        strcpy(stdoutFileName, constStdoutFileName);
-        strcpy(stderrFileName, constStderrFileName);
-        fflush(stdout);
-        fflush(stderr);
-        stdoutBackup = dup(1);
-        stderrBackup = dup(2);
+        if (separateStderr)
+        {
+            stdoutTempFileName = tempFilePrefix + tempFileSep + "stdout" + tempFileExtension;
+            stderrTempFileName = tempFilePrefix + tempFileSep + "stderr" + tempFileExtension;
 
-        stdoutFile = mkstemp(stdoutFileName);
-        dup2(stdoutFile, 1);
-#ifdef LOGGER_MERGE_STDERR_TO_STDOUT
-        dup2(stdoutFile, 2);
-#else
-        stderrFile = mkstemp(stderrFileName);
-        dup2(stderrFile, 2);
-        close(stderrFile);
-#endif // LOGGER_MERGE_STDERR_TO_STDOUT
-        close(stdoutFile);
+            stdoutTempFile = fopen(stdoutTempFileName.c_str(), creatingTempFileMode.c_str());
+            stderrTempFile = fopen(stderrTempFileName.c_str(), creatingTempFileMode.c_str());
+        }
+        else
+        {
+            stdoutAndStderrTempFileName = tempFilePrefix + tempFileSep + "stdoutAndStderr" + tempFileSep + tempFileExtension;
+            stdoutAndStderrTempFile = fopen(stdoutAndStderrTempFileName.c_str(), creatingTempFileMode.c_str());
+        }
+        if (!temp_files_are_open()) // TODO: add print from errno
+            throw runtime_error("Can't open temp files");
     }
 
-    void TearDown()
+    ~OutputRedirection()
+    {
+        if (separateStderr)
+        {
+            fclose(stdoutTempFile);
+            fclose(stderrTempFile);
+        }
+        else
+        {
+            fclose(stdoutAndStderrTempFile);
+        }
+
+        if (!keepTempFiles)
+        {
+            if(separateStderr)
+            {
+                remove(stdoutTempFileName.c_str());
+                remove(stderrTempFileName.c_str());
+            }
+            else
+            {
+                remove(stdoutAndStderrTempFileName.c_str());
+            }
+        }
+    }
+
+    void start_redirection()
     {
         fflush(stdout);
-        dup2(stdoutBackup, 1);
-        close(stdoutBackup);
-
         fflush(stderr);
-        dup2(stderrBackup, 2);
-        close(stderrBackup);
+        stdoutFDBackup = dup(1);
+        stderrFDBackup = dup(2);
 
-        if (UnitTest::GetInstance()->current_test_info()->result()->Failed())
+        if (separateStderr)
         {
-            ifstream f(stdoutFileName);
+            stdoutFD = fileno(stdoutTempFile);
+            stderrFD = fileno(stderrTempFile);
+            dup2(stdoutFD, 1);
+            dup2(stderrFD, 2);
+            close(stdoutFD);
+            close(stderrFD);
+        }
+        else
+        {
+            stdoutAndStderrFD = fileno(stdoutAndStderrTempFile);
+            dup2(stdoutAndStderrFD, 1);
+            dup2(stdoutAndStderrFD, 2);
+            close(stdoutAndStderrFD);
+        }
+    }
+
+    void stop_redirection()
+    {
+        fflush(stdout);
+        fflush(stderr);
+        dup2(stdoutFDBackup, 1);
+        dup2(stderrFDBackup, 2);
+        close(stdoutFDBackup);
+        close(stderrFDBackup);
+    }
+
+    void print()
+    {
+        if (!separateStderr)
+        {
+            ifstream f(stdoutAndStderrTempFileName);
             if (f.is_open())
                 cout << f.rdbuf();
         }
+    }
 
-        remove(stdoutFileName);
-#ifdef LOGGER_MERGE_STDERR_TO_STDOUT
-        remove(stderrFileName);
-#endif // LOGGER_MERGE_STDERR_TO_STDOUT
+private:
+    bool temp_files_are_open() const
+    {
+        if (separateStderr)
+            return stdoutTempFile && stderrTempFile;
+        else
+            return stdoutAndStderrTempFile;
     }
 };
 
@@ -70,17 +135,25 @@ class LoggerUnitTests : public ::testing::Test
 {
 protected:
 
-    OutputRedirection outputRedirection{};
+    OutputRedirection outputRedirection{string{} +
+        UnitTest::GetInstance()->current_test_suite()->name() +
+        OutputRedirection::tempFileSep +
+        UnitTest::GetInstance()->current_test_info()->name()};
 
     virtual void SetUp() override
     {
-        outputRedirection.SetUp();
+        outputRedirection.start_redirection();
         logger_init();
     }
 
     virtual void TearDown() override
     {
-        outputRedirection.TearDown();
+        outputRedirection.stop_redirection();
+
+        if (UnitTest::GetInstance()->current_test_info()->result()->Failed())
+        {
+            outputRedirection.print();
+        }
     }
 };
 
@@ -92,10 +165,10 @@ TEST_F(LoggerUnitTests, Test1) {
     FAIL();
 }
 
-// TEST_F(LoggerUnitTests, Test2) {
-//     // fprintf(stdout, "worldOut");
-//     // fprintf(stderr, "worldErr");
-// }
+TEST_F(LoggerUnitTests, Test2) {
+    fprintf(stdout, "worldOut");
+    fprintf(stderr, "worldErr");
+}
 
 int main(int argc, char *argv[])
 {
